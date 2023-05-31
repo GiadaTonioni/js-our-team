7a613631ea60b6ff1437285c6d8c5e5f2875c324
GiadaTonioni <giada.tonioni@gmail.com>
2023-05-31 13:58:20 +0200

    creato array di oggetti

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const team = [
+	{
+		'nome': 'Wayne Barnett',
+		'role' : 'Founder & CEO',
+		'immagine': './img/wayne-barnett-founder-ceo.jpg'
+	},
+	{
+		'nome': 'Angela Caroll',
+		'role' : 'Chief Editor',
+		'immagine': './img/angela-caroll-chief-editor.jpg'
+	},
+    {
+		'nome': 'Walter Gordon',
+		'role' : 'Office Manager',
+		'immagine': './img/walter-gordon-office-manager.jpg'
+	},
+    {
+		'nome': 'Angela Lopez',
+		'role' : 'Social Media Manager',
+		'immagine': './img/angela-lopez-social-media-manager.jpg'
+	},
+    {
+		'nome': 'Scott Estrada',
+		'role' : 'Developer',
+		'immagine': './img/scott-estrada-developer.jpg'
+	},
+    {
+		'nome': 'Barbara Ramos',
+		'role' : 'Graphic Designer',
+		'immagine': './img/barbara-ramos-graphic-designer.jpg'
+	}
+
+]
+console.log(team)
+
+
+
+
+
